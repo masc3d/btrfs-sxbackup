e52577efe74966880044739a14dc9e62b5b6fdd9
masc3d <masc@disappear.de>
2015-11-08 12:25:42 +0100

    ADDED license header to all source files, fixing #19

--- btrfs_sxbackup/__init__.py
+++ btrfs_sxbackup/__init__.py
@@ -1,8 +1,13 @@
+# Copyright (c) 2014 Marco Schindler
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+
 __version__ = '0.5.8'
 __author__ = 'Marco Schindler'
 __email__ = 'masc@disappear.de'
 __maintainer__ = 'masc@disappear.de'
 __license__ = 'GPL'
 __copyright__ = 'Copyright 2014, Marco Schindler'
-
-
--- btrfs_sxbackup/retention.py
+++ btrfs_sxbackup/retention.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2014 Marco Schindler
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+
 import collections
 import logging
 import math
